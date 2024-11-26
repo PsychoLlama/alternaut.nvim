777cd497e02f96e27857bcb1302b2c6571bc9488
Jesse Gibson <JesseTheGibson@gmail.com>
2024-11-26 15:59:02 -0500

    Add modern comforts
    I'm about to migrate this stuff to lua. Alternaut has aged poorly and
    needs some love. Currently just adding some Lua support so I can work
    locally with types and all that jazz.

---
+++ .vimrc.lua
@@ -0,0 +1,17 @@
+local repo = vim.fn.expand('<sfile>:h')
+
+require('core.pkg').on_load(function(plugins)
+  return vim
+    .iter(plugins)
+    :map(function(plugin)
+      if plugin.name ~= 'alternaut.nvim' then
+        return plugin
+      end
+
+      return vim.tbl_extend('force', plugin, {
+        type = 'path',
+        source = repo,
+      })
+    end)
+    :totable()
+end)
