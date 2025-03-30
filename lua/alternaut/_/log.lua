3e1753a237c9d3b73c2a1ec30cb89a5ec928889a
Jesse Gibson <JesseTheGibson@gmail.com>
2025-03-29 21:53:27 -0400

    Improve logging tools
    Now logs support highlighting similar to the old Vimscript
    implementation.

--- lua/alternaut/_/log.lua
+++ lua/alternaut/_/log.lua
@@ -1,21 +1,49 @@
-local M = {}
+local log = {}
 
-local level = vim.log.levels
-local PLUGIN_PREFIX = 'alternaut: '
+--- Log a message with highlighting.
+--- This is a little gross. There's probably a better way to do it.
+--- @vararg string|alternaut._.LogHighlight
+function log.with_highlight(...)
+  local parts = vim.tbl_map(function(part)
+    if type(part) == 'string' then
+      return { hi = 'None', msg = part }
+    end
 
---- Log information
-function M.info(msg)
-  vim.notify(PLUGIN_PREFIX .. msg, level.INFO)
+    return part
+  end, { ... })
+
+  for _, part in ipairs(parts) do
+    vim.cmd.echohl(part.hi)
+    vim.cmd.echon(vim.json.encode(part.msg))
+  end
+
+  vim.cmd.echohl('None')
 end
 
---- Log a warning
-function M.warn(msg)
-  vim.notify(PLUGIN_PREFIX .. msg, level.WARN)
+--- Informational message
+--- @vararg string|alternaut._.LogHighlight
+function log.info(...)
+  log.with_highlight({ hi = 'Comment', msg = 'alternaut: ' }, ...)
 end
 
---- Log an error
-function M.error(msg)
-  vim.notify(PLUGIN_PREFIX .. msg, level.ERROR)
+--- Warning message
+--- @vararg string|alternaut._.LogHighlight
+function log.warn(...)
+  log.with_highlight({ hi = 'Type', msg = 'alternaut: ' }, ...)
 end
 
-return M
+--- Error message
+--- @vararg string|alternaut._.LogHighlight
+function log.error(...)
+  log.with_highlight({ hi = 'Error', msg = 'alternaut: ' }, ...)
+end
+
+return log
+
+--- @class alternaut._.LogHighlight
+---
+--- Syntax highlight name.
+--- @field hi string
+---
+--- Message to display.
+--- @field msg string
